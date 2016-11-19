07b77df89907bb89fd7bc748af3bde31a9e2f067
Artemii Yanushevskyi <argoniton@Artemiis-MBP.Dlink>
2016-11-19 18:23:31 +0200

    new

--- xmlThing.py
+++ xmlThing.py
@@ -1,14 +1,21 @@
 def dictionaryToXML(dictionary):
     returnXML = ''
+    def escape(string):
+        ''' made to convert strings supported by xml'''
+        string = string.replace("<", "&lt;")
+        string = string.replace(">", "&gt;")
+        string = string.replace("&", "&amp;")
+        string = string.replace("\"", "&quot;")
+        return string
     def next(n, dictionary):
         returnElement = ''
         for e in dictionary.keys():
             if type(dictionary[e]) == type({}):
-                returnElement += '{}<dictionary title="{}">'.format('  '*n,e) + '\n'
+                returnElement += '{}<dictionary title="{}">'.format('  '*n, escape(e)) + '\n'
                 returnElement += next(n + 1, dictionary[e])
                 returnElement += '{}</dictionary>'.format('  '*n) + '\n'
             else:
-                returnElement += '{}<element title="{}">'.format('  '*n, e) + str(dictionary[e]) + '</element>' + '\n'
+                returnElement += '{}<element title="{}">'.format('  '*n, escape(e)) + escape(str(dictionary[e])) + '</element>' + '\n'
         return returnElement
     returnXML += '<?xml version="1.0" encoding="UTF-8"?>' + '\n'
     returnXML += '<top>' + '\n'
